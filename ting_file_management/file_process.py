431a33eb141064d475e2f0ab0f65ef6d1e7b6dc3
euHec <hectorsouzass@gmail.com>
2024-01-11 22:31:59 -0300

    solved process

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+from ting_file_management.file_management import txt_importer
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    if any(item["nome_do_arquivo"] == path_file for item in instance.queue):
+        return None
+
+    new_text = txt_importer(path_file)
+    new_dict = {
+        "nome_do_arquivo": path_file,
+        "qtd_linhas": len(new_text),
+        "linhas_do_arquivo": new_text,
+    }
+
+    instance.enqueue(new_dict)
+
+    print(new_dict)
 
 
 def remove(instance):

8bf919b500b90d1f26a0aecd3ec78dbb2bd34f5e
euHec <hectorsouzass@gmail.com>
2024-01-11 21:42:53 -0300

    solved txt_importer

--- ting_file_management/file_management.py
+++ ting_file_management/file_management.py
@@ -1,2 +1,17 @@
+import sys
+
+
 def txt_importer(path_file):
-    """Aqui irá sua implementação"""
+    try:
+        if not path_file.endswith(".txt"):
+            raise ValueError("Formato inválido")
+
+        with open(path_file, "r") as file:
+            list_of_files = [line.rstrip() for line in file.readlines()]
+            return list_of_files
+
+    except FileNotFoundError:
+        print(f"Arquivo {path_file} não encontrado", file=sys.stderr)
+
+    except ValueError as e:
+        print(e, file=sys.stderr)

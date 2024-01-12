c83a149c07db28264ad33c0a32665a30aca63051
euHec <hectorsouzass@gmail.com>
2024-01-11 23:52:44 -0300

    solved word_search

--- ting_word_searches/word_search.py
+++ ting_word_searches/word_search.py
@@ -1,6 +1,52 @@
 def exists_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = instance.queue
+    occurrences_list = list()
+
+    for file_info in result:
+        file_name = file_info["nome_do_arquivo"]
+        file_lines = file_info["linhas_do_arquivo"]
+
+        file_occurrences = list()
+
+        for line_number, line in enumerate(file_lines, start=1):
+            if word.lower() in line.lower():
+                file_occurrences.append({"linha": line_number})
+
+        if file_occurrences:
+            occurrences_list.append(
+                {
+                    "palavra": word,
+                    "arquivo": file_name,
+                    "ocorrencias": file_occurrences,
+                }
+            )
+
+    return occurrences_list
 
 
 def search_by_word(word, instance):
-    """Aqui irá sua implementação"""
+    result = instance.queue
+    occurrences_list = list()
+
+    for file_info in result:
+        file_name = file_info["nome_do_arquivo"]
+        file_lines = file_info["linhas_do_arquivo"]
+
+        file_occurrences = list()
+
+        for line_number, line in enumerate(file_lines, start=1):
+            if word.lower() in line.lower():
+                file_occurrences.append(
+                    {"linha": line_number, "conteudo": line}
+                )
+
+        if file_occurrences:
+            occurrences_list.append(
+                {
+                    "palavra": word,
+                    "arquivo": file_name,
+                    "ocorrencias": file_occurrences,
+                }
+            )
+
+    return occurrences_list
